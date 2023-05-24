388b06fb32ad8201645bf067be5b6559ede0bb1e
Krecik <bartoszkraczek2006@gmail.com>
2023-05-24 20:46:03 +0200

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
-import logo from '../assets'
+import { logo } from '../assets'
 
 
 const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
+    return (
+        <header className='w-full flex justify-center items-center flex-col'>
+            <nav className='flex justify-between items-center w-full mb-10 pt-3'>
+                <img src={logo} alt='logo'
+                    className='w-28 object-contain' />
+
+                <button
+                    type='button'
+                    onClick={() => window.open('https://github.com/KraczeX')}
+                    className='black_btn'>
+                    GitHub
+                </button>
+            </nav>
+
+        <h1 className='head_text'>Skróć Tekst dzięki <br className='max-md:hidden'/>
+        <span className='orange_gradient'> OpenAI GPT-4</span> </h1>
+        <h2 className='desc'>Uprość czytanie dzięki Summize, 
+            który przekształca długie artykuły w
+            jasne i zwięzłe streszczenia</h2>
+        </header>
+    )
 }
 
 export default Hero
